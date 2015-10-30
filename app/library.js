758ad087e7083e3b3e63d0be9229d69b26906be1
jburet <j.buret@mektoube.fr>
2015-10-30 14:21:15 +0100

    music play now.

--- app/library.js
+++ app/library.js
@@ -1,15 +1,21 @@
 'use strict';
 
+var conf = {
+    host: '149.202.44.123',
+    filesBucket: 'filesForUs'
+};
+
+var N1qlQuery = require('couchbase').N1qlQuery;
+
 var
-    low             = require('lowdb'),
-    dbFile          = low('databases/files.json'),
+    couchbase   = require('couchbase'),
+    Cluster     = new couchbase.Cluster(conf.host),
     sock            = require('./socket'),
     accepted_mimes  = [
         'audio/mp3'
     ];
 
 exports.handle = function(file){
-    console.log(file);
     if(accepted_mimes.indexOf(file.mimetype) === -1){
         return false;
     }else{
@@ -18,10 +24,40 @@ exports.handle = function(file){
             path : '/' + file.path,
             size : file.size,
             date : new Date(),
-            type : file.mimetype
+            type : file.mimetype,
         };
-        dbFile('files').push(obj);
-        sock.files();
-        return true;
+        var Bucket = Cluster.openBucket(conf.filesBucket, function(err){
+            if(err) console.log(err);
+        });
+        Bucket.insert(obj.name, obj, function(err, res) {
+            if (err) console.log(err);
+            else{
+                console.log(res);
+                exports.all();
+                return true;
+            }
+        });
     }
+};
+
+exports.play = function(file){
+    console.log(file.name);
+};
+
+exports.all = function(){
+    var files = [];
+    var ViewQuery = couchbase.ViewQuery;
+    var bucket = Cluster.openBucket(conf.filesBucket);
+    var q = ViewQuery.from('listing', 'allNames');
+    bucket.query(q, function(err, res){
+        if(err){
+            console.log("err");
+            console.log(err);
+        }else{
+            res.forEach(function(one){
+                files.push(one.value);
+            });
+            sock.files(files);
+        }
+    });
 };
