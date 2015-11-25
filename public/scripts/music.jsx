3b459fc93b0fd1c52c0ed42352446d8db2e57215
jburet <j.buret@mektoube.fr>
2015-11-25 09:59:24 +0100

    dl song

--- public/scripts/music.jsx
+++ public/scripts/music.jsx
@@ -8,7 +8,7 @@ var MusicItem = React.createClass({
         this.props.onWishPlay(this.props.src, this.props.type, this.props.meta);
     },
     showMenu: function(e){
-        this.props.showOnTop({x: e.pageX, y: e.pageY}, this.props.meta);
+        this.props.showOnTop({x: e.pageX, y: e.pageY}, this.props.meta, this.props.src, this.props.name);
     },
     render: function(){
         return (
@@ -23,7 +23,11 @@ var MusicItem = React.createClass({
                         <img src={'http://azerty.gq' + (this.props.meta.picture || '/img/default_cover.png')} alt="cover" className="cov"/>
                     </div>
                 </span>
-                <img className="itemMenu" src="img/ic_more_vert_black_24dp_1x.png" onClick={this.showMenu}/>
+                <img
+                    className="itemMenu"
+                    src="img/ic_more_vert_black_24dp_1x.png"
+                    onClick={this.showMenu}
+                />
             </li>
         );
     }
@@ -66,6 +70,15 @@ var ItemMenu = React.createClass({
             showOnTop : !this.state.showOnTop
         });
     },
+    download: function(){
+        console.log(this.props);
+        var dl = document.createElement('a');
+        dl.setAttribute('href', this.props.e.src);
+        dl.setAttribute('download', this.props.e.name);
+        dl.click();
+        dl = null;
+        this.props.closeMenu();
+    },
     componentDidMount: function(){
         $('#optsItem').css('top', this.props.e.e.y + 'px');
         $('#optsItem').css('left', this.props.e.e.x + 'px');
@@ -74,7 +87,7 @@ var ItemMenu = React.createClass({
         return(
             <span>
                 <div id="optsItem">
-                    <span className="oneOpt" onClick={this.showOnTop}><img src="img/ic_file_download_black_24dp.png" alt="dl" className="imgOpt"/><span className="txtOpt">Download</span></span>
+                    <span className="oneOpt" onClick={this.download}><img src="img/ic_file_download_black_24dp.png" alt="dl" className="imgOpt"/><span className="txtOpt">Download</span></span>
                     <span className="oneOpt" onClick={this.showOnTop}><img src="img/ic_edit_black_24dp.png" alt="edit" className="imgOpt"/><span className="txtOpt">Edit</span></span>
                     <span className="oneOpt" onClick={this.showOnTop}><img src="img/ic_comment_black_24dp.png" alt="edit" className="imgOpt"/><span className="txtOpt">Comment</span></span>
                     <span className="oneOpt" onClick={this.showOnTop}><img src="img/ic_delete_black_24dp.png" alt="del" className="imgOpt"/><span className="txtOpt">Delete</span></span>
@@ -100,11 +113,14 @@ exports.MusicBox = React.createClass({
             showMenu: false
         };
     },
-    showOnTop: function(e, meta){
+    showOnTop: function(e, meta, src, name){
         this.setState({
-            toTop: {meta: meta, e: e},
+            toTop: {meta: meta, e: e, src:src, name: name},
             showMenu : !this.state.showMenu
         });
+    },
+    closeMenu: function(){
+        this.setState({showMenu: false});
     },
     render: function(){
         var musicNodes;
@@ -114,6 +130,7 @@ exports.MusicBox = React.createClass({
                     <MusicItem
                         key={music.id}
                         meta={music.meta}
+                        name={music.name}
                         src={music.path}
                         type={music.type}
                         onWishPlay={this.props.play}
@@ -131,6 +148,7 @@ exports.MusicBox = React.createClass({
                     this.state.showMenu ?
                         <ItemMenu
                             e={this.state.toTop}
+                            closeMenu={this.closeMenu}
                         />
                     : null
                 }
