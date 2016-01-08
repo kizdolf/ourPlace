a9725884b74ac3d59229af81023e2568e34c13aa
Kizdolf <kizdolf.aez@gmail.com>
2016-01-08 10:29:13 +0100

    rooting is on his waaayyyy

--- public/scripts/root.jsx
+++ public/scripts/root.jsx
@@ -4,10 +4,26 @@ var
 
 
 exports.RootBox = React.createClass({
+    amIRoot: function(cb){
+        $.get('/api/root/amI', (res)=>{
+            cb(res);
+        });
+    },
+    getAllUsers: function(){
+        $.get('/api/root/users/all', (res)=>{
+            console.log(res);
+        });
+    },
+    componentDidMount: function(){
+        this.amIRoot((amI)=>{
+            if(!amI) window.location.href = '/';
+            this.getAllUsers();
+        });
+    },
     render: function(){
         return (
             <div>
-                <h2>If you're not root, nothing we'll be possible. nope.</h2>
+                
             </div>
         );
     }
