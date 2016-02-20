2aa021f76b8aaa8da95e9c9762d617bb6ae74114
Kizdolf <kizdolf.aez@gmail.com>
2016-02-20 15:51:51 +0100

    add critical conf example.

---
+++ app/example.criticalConf.js
@@ -0,0 +1,22 @@
+'use strict';
+
+exports.connect =  {
+    db: 'dbName',
+    host: 'localhost',
+    port: 28015, //default one
+    authKey: 'the secret key you did set up'
+};
+
+exports.local = {
+	appPath: '/path/to/app' //(eg: /home/user/ourPlace). DO Not leave the trailing slash.
+};
+
+exports.https = {
+    privKey: '/path/to/certificates/privkey.pem',
+    certificate: '/path/to/certificates/cert.pem',
+    chain : '/path/to/certificates/chain.pem'
+};
+
+exports.extern = {
+	mandrillApiKey: 'mandrillApiKey' //=>https://mandrillapp.com
+};
